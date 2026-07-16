745393f9ed02d38f2a122c71d5e193ed0dac21f6
OneLoneDeveloper <mariogamer329@gmail.com>
2026-07-15 23:05:18 -0700

    remove unused code

--- public/js/main.js
+++ public/js/main.js
@@ -5,28 +5,3 @@ import { initAccordion } from "./components/accordion.js";
 initNavbar();
 initModal();
 initAccordion();
-
-// Tell the CSS that JavaScript is available.
-document.documentElement.classList.replace("no-js", "js");
-
-const menuButton = document.querySelector("[data-menu-toggle]");
-const navigation = document.querySelector("[data-navigation]");
-
-if (menuButton && navigation) {
-  menuButton.addEventListener("click", () => {
-    const isOpen = navigation.classList.toggle("is-open");
-
-    menuButton.setAttribute("aria-expanded", String(isOpen));
-  });
-
-  navigation.addEventListener("click", (event) => {
-    const clickedLink = event.target.closest("a");
-
-    if (!clickedLink) {
-      return;
-    }
-
-    navigation.classList.remove("is-open");
-    menuButton.setAttribute("aria-expanded", "false");
-  });
-}
